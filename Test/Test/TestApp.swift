cb080f5c76a9d56c992bf7f44024dfd75d26fb5c
danilone123 <danielvelascozarate@gmail.com>
2023-09-07 22:50:14 -0400

    creating Platzi test project

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by danny on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
